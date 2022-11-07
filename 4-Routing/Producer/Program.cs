0b3bb758b0c5af11ae03c327272a50522e05981c
mike zhang <mike.zhang@dpworld.com>
2022-11-07 13:25:32 -0800

    update for mac m1

--- 4-Routing/Producer/Program.cs
+++ 4-Routing/Producer/Program.cs
@@ -11,9 +11,14 @@ using var channel = connection.CreateModel();
 channel.ExchangeDeclare(exchange: "routing", type: ExchangeType.Direct);
 
 var message = "This message needs to be routed";
+var message2 = "This message routed for both";
 
 var body = Encoding.UTF8.GetBytes(message);
+var body2 = Encoding.UTF8.GetBytes(message2);
+
 
 channel.BasicPublish(exchange: "routing", routingKey: "analyticsonly", null, body);
+channel.BasicPublish(exchange: "routing", routingKey: "both", null, body2);
 
 Console.WriteLine($"Send message: {message}");
+Console.WriteLine($"Send message: {message2}");
